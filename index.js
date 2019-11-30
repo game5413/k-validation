f7e94207e4693f47389007d3c2ef3297d9b2d9e5
febryanph <febryanph10@gmail.com>
2019-11-30 19:46:44 +0700

    fix required for date

--- index.js
+++ index.js
@@ -169,11 +169,13 @@
     
                 function phone() {
                     _RULES_PARAMETER = /^0[1-9]\d+/g
+                    _RULES_PARAMETER = _RULES_PARAMETER.toString()
                     return regex()
                 }
     
                 function email() {
                     _RULES_PARAMETER = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
+                    _RULES_PARAMETER = _RULES_PARAMETER.toString()
                     return regex()
                 }
     
@@ -258,7 +260,7 @@
                                 var _FIELD = _CUSTOM_MESSAGE.slice()
                                 _CUSTOM_MESSAGE = _MESSAGE.replace(/{{field}}/g, _FIELD)
                             } else _CUSTOM_MESSAGE = ''
-                            _VALUE = _VALUE.value
+                            if (!_VALUE instanceof Date) _VALUE = _VALUE.value
                         }
                         // console.log(_ARRAY_OF_RULES)
                         _FUNCTION = _FUNCTION()
